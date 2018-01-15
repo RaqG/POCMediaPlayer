142fc497450c78acae5baa858849932a243337eb
Raquel Gallo <raqbgallo@gmail.com>
2018-01-15 15:25:32 -0200

    Initial commit

---
+++ app/src/main/java/com/example/raquelgallo/pocmediaaudio/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.raquelgallo.pocmediaaudio;
+
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    float volume = 0.05f;
+
+    @BindView(R.id.parent_view)
+    public LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.revenge_spoon);
+
+        if(savedInstanceState != null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.revenge_spoon);
+            volume = savedInstanceState.getFloat("volume");
+            mediaPlayer.seekTo(savedInstanceState.getInt("songPosition"));
+            startAudio();
+        }
+
+        mediaPlayer.setVolume(volume, volume);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        pauseAudio();
+        outState.putFloat("volume", volume);
+        if(mediaPlayer == null)
+            outState.putInt("songPosition", 0);
+        else
+            outState.putInt("songPosition", mediaPlayer.getCurrentPosition());
+    }
+
+    @OnClick(R.id.start_btn)
+    public void startAudio() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.revenge_spoon);
+            mediaPlayer.setVolume(volume, volume);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build());
+        }
+        mediaPlayer.start();
+
+    }
+
+    @OnClick(R.id.pause_btn)
+    public void pauseAudio() {
+        if (mediaPlayer != null)
+            mediaPlayer.pause();
+    }
+
+    @OnClick(R.id.stop_btn)
+    public void stopAudio() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        volume = 0.05f;
+    }
+
+    @OnClick(R.id.up_volume_btn)
+    public void upVolume() {
+        if (mediaPlayer != null) {
+            if (volume <= 1.0f) {
+                volume += 0.01f;
+                mediaPlayer.setVolume(volume, volume);
+            } else {
+                Snackbar.make(linearLayout, "Volume máximo!", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @OnClick(R.id.down_volume_btn)
+    public void downVolume() {
+        if (mediaPlayer != null) {
+            if (volume >= 0.0f) {
+                volume -= 0.01f;
+                mediaPlayer.setVolume(volume, volume);
+            } else {
+                Snackbar.make(linearLayout, "Volume mínimo!", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+}
